11a700ee72477cb869d617867313370836953245
Aliya Rahman <aliya.rahman@yahoo.com>
2016-02-18 13:25:59 -0500

    Updated front end for all pages

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	url(r'^request_delivery/$', views.request_delivery, name='request_delivery'),
 	url(r'^organization_offer/$', views.organization_offer, name='organization_offer'),
 	url(r'^individual_offer/$', views.individual_offer, name='individual_offer'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^about/$', views.about, name='about'),
     url(r'^faq/$', views.faq, name='faq'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,10 +3,25 @@ from geopy.distance import vincenty
 from geopy.geocoders import GoogleV3
 from app.models import *
 from app.forms import AddDeliveryDateForm, RequestDeliveryForm, IndividualOfferForm, OrganizationForm, DistributionEventForm
+import calendar
+from datetime import date
 
 def index(request):
-    locations = Organization.objects.all()
-    events = DistributionEvent.objects.all()
+    weekday = calendar.day_name[date.today().weekday()]
+    if weekday=='Monday':
+        locations = Organization.objects.exclude(monday_dist_start='')
+    if weekday=='Tuesday':
+        locations = Organization.objects.exclude(tuesday_dist_start='')
+    if weekday=='Wednesday':
+        locations = Organization.objects.exclude(wednesday_dist_start='')
+    if weekday=='Thursday':
+        locations = Organization.objects.exclude(thursday_dist_start='')
+    if weekday=='Friday':
+        locations = Organization.objects.exclude(friday_dist_start='')
+    if weekday=='Saturday':
+        locations = Organization.objects.exclude(saturday_dist_start='')
+    if weekday=='Sunday':
+        locs = Organization.objects.exclude(sunday_dist_start='')
     return render(request, "index.html", {'locations' : locations})
 
 
@@ -21,9 +36,12 @@ def organization_offer(request):
 
 
 def individual_offer(request):
-    form = OrganizationOfferForm()
+    form = OrganizationForm()
     return render(request, "individual_offer.html", {'form' : form})
 
+def search(request):
+    locations = Organization.objects.all()
+    return render(request, "search.html", {'locations' : locations})
 
 def about(request):
     return render(request, "about.html")
